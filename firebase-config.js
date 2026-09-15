814966784a3b45670414621d03f28eecbacb0d95
my new codes <s300117950@ddsbstudent.ca>
2026-09-15 10:07:19 -0400

    Add browser Firebase configuration template

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Fill these values from Firebase Console > Project settings > Your apps > Web app.
+// Firebase web config is designed to be used by browser clients. Do not put server/admin credentials here.
+export const firebaseConfig = {
+  apiKey: "PASTE_YOUR_FIREBASE_API_KEY",
+  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
+  databaseURL: "https://PASTE_YOUR_DATABASE.firebasedatabase.app",
+  projectId: "PASTE_YOUR_PROJECT_ID",
+  storageBucket: "PASTE_YOUR_PROJECT.firebasestorage.app",
+  messagingSenderId: "PASTE_YOUR_SENDER_ID",
+  appId: "PASTE_YOUR_APP_ID"
+};
